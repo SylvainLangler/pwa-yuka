bf0e2d09dd59417b554512fa841c5f00b9a99706
SylvainLangler <langlersylvain@gmail.com>
2019-12-19 15:30:53 +0100

    start project

---
+++ pages/products.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>Hey</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default{
+
+    mounted(){
+        fetchProducts()
+    },
+    computed: mapState({
+        products: state => state.api
+    }),
+    methods: {
+        ...mapActions(['fetchProducts'])
+    }
+}
+</script>
+
+<style>
+
+</style>
