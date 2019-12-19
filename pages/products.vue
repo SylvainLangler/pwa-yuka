3b2d5be88a4f10e30126c2ba6055060f597d3539
SylvainLangler <langlersylvain@gmail.com>
2019-12-19 16:28:32 +0100

    début liste produits

--- pages/products.vue
+++ pages/products.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>Hey</div>
+    <div class="products">
+        <div v-for='(product,index) in products' :key='index' class="product">
+            <div class="img-product">
+                <img v-bind:src="product['photo']">
+            </div>
+            <div>{{ product['name'] }}</div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,18 +14,43 @@ import { mapActions } from 'vuex'
 
 export default{
 
-    mounted(){
-        fetchProducts()
+    data: function(){
+        return{
+            products: [
+                {name: "Lipton Ice Tea", photo: "/img/lipton_icetea.jpg"},
+                {name: "Coca cola", photo: "/img/lipton_icetea.jpg"},
+                {name: "Fanta citron", photo: "/img/lipton_icetea.jpg"},
+                {name: "Jus de pomme", photo: "/img/lipton_icetea.jpg"}
+            ]
+        }
     },
-    computed: mapState({
-        products: state => state.api
-    }),
+    
+    mounted(){
+    },
+
     methods: {
-        ...mapActions(['fetchProducts'])
+        
     }
 }
 </script>
 
 <style>
+.products{
+    display:flex;
+    flex-direction:column;
+}
+.product{
+    display:flex;
+    padding:15px;
+    border-bottom: 1px solid grey;
+    width:90%;
+    align-self:center;
+}
+.img-product{
+    width:30%;
+}
 
+.img-product img{
+    width:100%;
+}
 </style>
